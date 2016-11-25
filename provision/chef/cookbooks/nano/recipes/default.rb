396b42d0d5f0dbd34519961eb5742de8a314085f
STiago <victoriasantiago@correo.ugr.es>
2016-11-26 00:38:35 +0100

    Modificacion

--- provision/chef/cookbooks/nano/recipes/default.rb
+++ provision/chef/cookbooks/nano/recipes/default.rb
@@ -1,9 +1,8 @@
-package 'nano'
-directory '/home/ubuntu/Lecrin_Bot/provision/chef'
-file "/home/ubuntu/Lecrin_Bot/provision/chef/LEEME" do
-	owner "ubuntu"
-	group "ubuntu"
-	mode 00544
-	action :create
-	content "Directorio para documentos"
-end
+apt_update 'all platforms'
+
+package 'mongodb'
+package 'python-setuptools'
+package 'python-dev'
+package 'python-pip'
+
+easy_install_package 'python-telegram-bot'
