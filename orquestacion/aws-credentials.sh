395475ab847d8a50e5c99f98451f6c11a1a96f34
STiago <victoriasantiago@correo.ugr.es>
2016-12-13 20:24:49 +0100

    Aadiendo ficheros de orquestación

--- orquestacion/aws-credentials.sh
+++ orquestacion/aws-credentials.sh
@@ -1,5 +1,5 @@
-export AWS_KEY=AKIAIMLAZP4WIN3Y2PJA
-export AWS_SECRET=g8mOeWLcHbVs9ZjLaWho
-export AWS_SECURITY_GROUP_NAME=default
+export AWS_KEY=AKIAJQHOPQKCVARNSJAA
+export AWS_SECRET=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
+export AWS_SECURITY_GROUP_NAME=launch-wizard-2
 export AWS_KEYNAME=victoriakey
 export AWS_KEYPATH=/home/victoria/.ssh/victoriakey.pem
