606da035f822a9ee89bf53f45de77e3f58c6139c
STiago <victoriasantiago@correo.ugr.es>
2016-12-13 10:35:16 +0100

    Añadiendo ficheros

---
+++ orquestacion/aws-credentials.sh
@@ -0,0 +1,5 @@
+export AWS_KEY=AKIAIMLAZP4WIN3Y2PJA
+export AWS_SECRET=g8mOeWLcHbVs9ZjLaWho
+export AWS_SECURITY_GROUP_NAME=default
+export AWS_KEYNAME=victoriakey
+export AWS_KEYPATH=/home/victoria/.ssh/victoriakey.pem
